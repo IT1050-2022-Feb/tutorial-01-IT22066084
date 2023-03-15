5e8a026f26ed35a6c34088c30cc50b62eb3aab1b
IT22066084 <127924266+IT22066084@users.noreply.github.com>
2023-03-15 06:28:38 +0000

    What did you change ?

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,24 @@
 #include <stdio.h>
 
 int main() {
-  hhk.k/.g
+//declaring variables
+  int m1,m2,sum;
+  float average;
+  
+// get user input 
+  printf("Enter mark 1 : ");
+  scanf("%d",&m1);
+
+  printf("Enter mark 2 : ");
+  scanf("%d",&m2);
+//Calculating sum
+  sum = m1 + m2;
+//calculating average
+  average = sum/2.0;
+//display average
+  printf("The average is %.2f",average);
+  
+
   
   return 0;
 }
